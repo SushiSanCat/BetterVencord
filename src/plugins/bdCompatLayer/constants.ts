2c60715b17f8dc4a9d0e52e9598296c7cc15adf0
Daniel <davilareko@gmail.com>
2025-03-24 13:04:19 +0100

    Use new ZenFS build
    https://github.com/LosersUnited/ZenFS-builds/commit/e218039f6649fed2a1fb7292059ad966e240efb9

--- src/plugins/bdCompatLayer/constants.ts
+++ src/plugins/bdCompatLayer/constants.ts
@@ -6,4 +6,4 @@
 
 export const PLUGIN_NAME = "BD Compatibility Layer";
 export const BROWSERFS_BUILD_HASH = "1424d8eb5a28610e64fc8bde305eeb0676c9667e";
-export const ZENFS_BUILD_HASH = "b7204fb87b6ea1a56c8f80dffde31765015e0b6f";
+export const ZENFS_BUILD_HASH = "e218039f6649fed2a1fb7292059ad966e240efb9";
